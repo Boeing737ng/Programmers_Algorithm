4f60e71a1392d1f376b09663e35153e665fc1d49
Boeing737ng <ghlove0317@naver.com>
2018-06-19 23:53:54 +0900

    콜라츠 추측

---
+++ Level.1/콜라츠 추측/main.py
@@ -0,0 +1,13 @@
+def solution(num):
+    answer = 0
+    while True:
+        if num == 1:
+            return answer
+        answer += 1
+        if answer == 500:
+            return -1
+        if num % 2 == 0:
+            num = num / 2
+        else:
+            num = ((num * 3) + 1)
+    return answer
